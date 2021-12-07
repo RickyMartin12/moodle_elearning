a75cc5930d71e5ed05ccb7362f2eba081b3911af
Ricardo Peleira <ricardopeleira16@gmail.com>
2021-12-07 19:02:33 +0000

    make it better

--- connect.php
+++ connect.php
@@ -3,10 +3,10 @@
 date_default_timezone_set('Europe/Lisbon');//or change to whatever timezone you want
 header('Content-Type: text/html; charset=UTF-8');
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "moodle";
+$servername = "us-cdbr-east-04.cleardb.com";
+$username = "bf11f62aeedaa0";
+$password = "d4a3e50d";
+$dbname = "heroku_f9fbbc4dd7ac956";
 
 
 
