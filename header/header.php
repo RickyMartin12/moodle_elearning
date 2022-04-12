dff932274d81075c56b1711ee8365036dd7b3e33
https://github.com/RickyMartin12/moodle_elearning.git <11knum15@gmail.com>
2022-04-12 18:31:17 +0100

    tes2346772345633444

--- header/header.php
+++ header/header.php
@@ -12,7 +12,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
 <link href="css/bootstrap.min.css" rel='stylesheet' type='text/css' />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/bootstrap-switch.min.css">
+  <link rel="stylesheet" href="<?php echo $_SERVER['DOCUMENT_ROOT']. "/css/bootstrap-switch.min.css"; ?>">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 <link href="css/w3.css" rel='stylesheet' type='text/css' />
 <link href="css/style.css" rel='stylesheet' type='text/css' />
