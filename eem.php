9a233a63e5e83e3adeb87e044cc0a5db83f391b5
https://github.com/RickyMartin12/moodle_elearning.git <11knum15@gmail.com>
2022-04-14 16:41:27 +0100

    cat4123112111222

--- eem.php
+++ eem.php
@@ -1,32 +1,34 @@
 <?php
-
-require $_SERVER['DOCUMENT_ROOT']. "/connect.php";
-
-$obter_comp=" SELECT nome FROM admins ORDER BY nome ASC";
-          $result = mysqli_query($conn, $obter_comp);
-          while($obj = mysqli_fetch_object($result)) 
-          {
-            $v[] = $obj;
-          }
-
-          $array = json_decode(json_encode($v), True);
-
-          var_dump($array);
-
-          $nome_utilizador = "Ricardo Peleira";
-
-          foreach($v as $index => $string) {
-            if (strpos($string, $nome_utilizador) !== false)
-            {
-              $r_p_string = true;
-            }
-            else
-            {
-              $r_p_string = false;
-            }
-          }
-
-          echo $r_p_string;
-
-
-          ?>
+require_once "vendor/autoload.php";
+ 
+use League\Flysystem\Filesystem;
+use League\Flysystem\Adapter\Ftp\FtpAdapter as Adapter;
+ 
+if (isset($_POST['submit'])) {
+    $adapter = new League\Flysystem\Ftp\FtpAdapter(
+        League\Flysystem\Ftp\FtpConnectionOptions::fromArray([
+            'host' => 'ftp.interhub.org',
+            'root' => '/',
+            'username' => 'interhub',
+            'password' => '+Kw[P~djRpuS',
+        ])
+    );
+ 
+    $filesystem = new Filesystem($adapter);
+ 
+    try {
+        $source = fopen($_FILES['image']['tmp_name'], 'r+');
+        $path = 'public_html/cat/'.$_FILES['image']['name'];
+        $filesystem->writeStream($path, $source);
+        fclose($source);
+        echo "File uploaded successfully.";
+    } catch (FilesystemError | UnableToWriteFile $exception) {
+        echo $exception->getMessage();
+    }
+}
+?>
+ 
+<form method="post" enctype="multipart/form-data">
+    <p><input type="file" name="image" /></p>
+    <input type="submit" name="submit" value="Submit">
+</form>
