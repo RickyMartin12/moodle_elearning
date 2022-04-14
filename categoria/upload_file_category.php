a3fa1ed93e5051ebe9720fba6372c5addb6a6625
https://github.com/RickyMartin12/moodle_elearning.git <11knum15@gmail.com>
2022-04-14 16:24:24 +0100

    cursos3

--- categoria/upload_file_category.php
+++ categoria/upload_file_category.php
@@ -1,6 +1,23 @@
 <?php
 
-    
+
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
+
+    use League\Flysystem\Filesystem;
+    use League\Flysystem\Adapter\Ftp\FtpAdapter as Adapter;
+
+
+    $adapter = new League\Flysystem\Ftp\FtpAdapter(
+        League\Flysystem\Ftp\FtpConnectionOptions::fromArray([
+            'host' => 'ftp.interhub.org',
+            'root' => '/',
+            'username' => 'interhub',
+            'password' => '+Kw[P~djRpuS',
+        ])
+    );
+ 
+    $filesystem = new Filesystem($adapter);
+
 
     $path="https://interhub.org/cat/";//server path
     foreach ($_FILES as $key) {
@@ -8,14 +25,19 @@
             $name = $key['name'];
             $temp = $key['tmp_name'];
             $size= ($key['size'] / 1000)."Kb";
-            move_uploaded_file($temp, $path . $name);
+            /*move_uploaded_file($temp, $path . $name);
             echo "
                 <div>
                     <h12><strong>File Name: $name</strong></h2><br />
                     <h12><strong>Size: $size</strong></h2><br />
                     <hr>
                 </div>
-                ";
+                ";*/
+
+                $source = fopen($temp, 'r+');
+                $path = 'public_html/cat/'.$name;
+                $filesystem->writeStream($path, $source);
+                fclose($source);
         }else{
             echo $key['error'];
         }
