683059888c89d9a1eba05e7b90379845210af328
https://github.com/RickyMartin12/moodle_elearning.git <11knum15@gmail.com>
2022-04-14 16:33:36 +0100

    cat4123112

--- categoria/upload_file_category.php
+++ categoria/upload_file_category.php
@@ -20,18 +20,13 @@
 
 
     foreach ($_FILES as $key) {
-        if($key['error'] == UPLOAD_ERR_OK ){
+
+        echo $key['name'];
+
+        /*if($key['error'] == UPLOAD_ERR_OK ){
             $name = $key['name'];
             $temp = $key['tmp_name'];
             $size= ($key['size'] / 1000)."Kb";
-            /*move_uploaded_file($temp, $path . $name);
-            echo "
-                <div>
-                    <h12><strong>File Name: $name</strong></h2><br />
-                    <h12><strong>Size: $size</strong></h2><br />
-                    <hr>
-                </div>
-                ";*/
 
                 try {
                     $source = fopen($temp, 'r+');
@@ -45,7 +40,7 @@
                 
         }else{
             echo $key['error'];
-        }
+        }*/
     } 
 
 ?>
