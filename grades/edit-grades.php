7d12e7a4ed63749195ba3b5dcea6e56f00b765cb
https://github.com/RickyMartin12/moodle_elearning.git <11knum15@gmail.com>
2022-10-19 21:48:50 +0100

    teste2

--- grades/edit-grades.php
+++ grades/edit-grades.php
@@ -194,7 +194,7 @@
 
 			        </div>
 			        <div class="modal-footer">
-			        	<p style='text-align:center; margin:0;'><img src="/moodle/images/cs.png" class="logo-nav" style="width:68px;"></p>
+			        	<p style='text-align:center; margin:0;'><img src="/images/cs.png" class="logo-nav" style="width:68px;"></p>
 				        <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times-circle" aria-hidden="true"> </i><font class="hidden-xs"> Cancel</font></button>
 	                    <button title="Guardar as alterações do blogue" type="button" class="btn btn-success" onclick="saveGrade($('#id_grade').html());"> <span class="glyphicon glyphicon-save-file"></span><font class="hidden-xs"> Save</font></button>
 			        </div>
@@ -344,7 +344,7 @@ function getCourses(cursos)
 
   setTimeout(function(){ 
   dataValue='action=4';
-    $.ajax({ url:'/moodle/cursos/action/action_cursos.php',
+    $.ajax({ url:'/cursos/action/action_cursos.php',
     data:dataValue,
     type:'POST', 
     cache:false,
@@ -394,7 +394,7 @@ function getSubCourse(nome_curso, sub_cursos)
 
   setTimeout(function(){ 
   dataValue='action=7&nome_curso='+escape(nome_curso);
-    $.ajax({ url:'/moodle/cursos/action/action_cursos.php',
+    $.ajax({ url:'/cursos/action/action_cursos.php',
     data:dataValue,
     type:'POST', 
     cache:false,
@@ -437,7 +437,7 @@ function getCoursesbyID(nome_curso, sub_nome_curso)
 {
   setTimeout(function(){
   dataValue='action=8&nome_curso='+escape(nome_curso)+'&sub_nome_curso='+escape(sub_nome_curso);
-    $.ajax({ url:'/moodle/cursos/action/action_cursos.php',
+    $.ajax({ url:'/cursos/action/action_cursos.php',
     data:dataValue,
     type:'POST', 
     cache:false,
@@ -608,6 +608,10 @@ function saveGrade(id)
 			  });
 
 
+			  
+			  
+			  
+			  
 
 	    		edit_grades.modal('hide');
 
